811d4a5f31ba2fefa8d994f2b4b20d198b5ca215
amare1990 <amaremek@gmail.com>
2022-11-22 22:15:43 +0300

    Modify configureStore.js file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 // import retrieveRocket from "./rockets/api/fetchRockets";
-import rocketsReducer from "./rockets/rockets";
-
+import rocketsReducer from './rockets/rockets';
+import missionsReducer from './missions/missions';
 
 const store = configureStore({
-  reducer: {rockets: rocketsReducer, missions: missionsReducer}
+  reducer: { rockets: rocketsReducer, missions: missionsReducer },
 });
 
 export default store;
