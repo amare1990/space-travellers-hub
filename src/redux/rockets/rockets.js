43e637289d6d3128989a8b48a45652f9a6656aea
amare1990 <amaremek@gmail.com>
2022-11-22 22:15:05 +0300

    Add missions reducer

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -2,7 +2,7 @@
 const GET_ROCKET = 'space-travellers-hub/src/redux/rockets/fetchRocket';
 
 const initialState = [];
-const sN = 0;
+// const sN = 0;
 
 // function that returns action
 
@@ -15,9 +15,9 @@ export default function rocketsReducer(state = initialState, action) {
   switch (action.type) {
     case action.rockets: {
       const rocketsArray = [];
-      action.rockets.forEach(rocket => rocketsArray.push(
-        {      
-          flickr_images: rocket.flickr_images,      
+      action.rockets.forEach((rocket) => rocketsArray.push(
+        {
+          flickr_images: rocket.flickr_images,
           description: rocket.description,
           rocket_id: rocket.rocket_id,
           rocket_name: rocket.rocket_name,
