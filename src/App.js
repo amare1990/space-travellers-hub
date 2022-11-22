4e5aadc0ddb80d39918634bef4836ebb2824f0a5
amare1990 <amaremek@gmail.com>
2022-11-22 21:43:43 +0300

    Modify rocketsReducer function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { useDispatch } from 'react-redux';
 import NavBar from './pages/Nav';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import Profile from './pages/Profile';
-import { useDispatch } from 'react-redux';
 import { fetchRockets } from './redux/rockets/rockets';
 
 function App() {
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -15,63 +15,13 @@ export default function rocketsReducer(state = initialState, action) {
   switch (action.type) {
     case action.rockets: {
       const rocketsArray = [];
-      Object.entries(action.rockets).forEach(([key, value]) => rocketsArray.push(
-        { sN: sN + 1 }, [{
-          id: key,
-          active,
-          stages,
-          boosters,
-          cost_per_launch,
-          success_rate_pct,
-          first_flight,
-          country,
-          comapny,
-          height: { metres, feet },
-          diameter: { meters, feet },
-          mass: { kg, lb },
-          payload_weights: [{
-            id, name, kg, lb,
-          }],
-          first_stage: {
-            reusable,
-            engines,
-            fuel_amount_tons,
-            burn_time_sec,
-            thrust_sea_level: { kN, lbf },
-            thrust_vacuum: { kN, lbf },
-          },
-          second_stage: {
-            reusable,
-            engines,
-            fuel_amount_tons,
-            burn_time_sec,
-            thrust: { kN, lbf },
-            payloads: {
-              option_1,
-              composite_fairing: { height: { meters, feet }, diameter: { meters, feet } },
-            },
-          },
-          engines: {
-            number,
-            type,
-            version,
-            layout,
-            isp: { sea_level, vaccum },
-            engine_loss_max,
-            popellant_1,
-            popellant_2,
-            thrust_sea_level: { kN, lbf },
-            thrust_vacuum: { kN, lbf },
-            thrust_to_weight,
-          },
-          landing_legs: { number, material },
-          flickr_images: [],
-          wikipedia,
-          description,
-          rocket_id,
-          rocket_name,
-          rocket_type,
-        }],
+      action.rockets.forEach(rocket => rocketsArray.push(
+        {      
+          flickr_images: rocket.flickr_images,      
+          description: rocket.description,
+          rocket_id: rocket.rocket_id,
+          rocket_name: rocket.rocket_name,
+        },
       ));
       return [...rocketsArray];
     }
