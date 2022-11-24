371a5fc4d1303d29bafac4e018d0f29bca1f68c0
amare1990 <amaremek@gmail.com>
2022-11-24 21:48:32 +0300

    Update the My portfolio page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rockets';
 
-const Profile = () => (
-  <div className="Profile" />
-);
+const Profile = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (rockets.length === 0) {
+      dispatch(fetchRockets());
+    }
+  });
+
+    <div className="Profile">
+      {
+      rockets.forEach((rocket) => (
+        rocket.rocket_id ? (
+          <li key={rocket.rocket_id}>
+            rocket.rocket_name
+          </li>
+        ) : "Yo didn't reserve any rocket yet"
+      ))
+        }
+    </div>;
+};
 
 export default Profile;
